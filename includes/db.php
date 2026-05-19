6c4d89d41612780b9b3226da85b9e75105d291a0
Pranav <pranavbairy2@gmail.com>
2026-05-19 09:39:16 +0530

    Added: Persistent storage

--- includes/db.php
+++ includes/db.php
@@ -21,6 +21,7 @@ function get_db(): PDO {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_PERSISTENT         => true,
         ]);
     }
     return $pdo;
